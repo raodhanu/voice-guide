e27032d84a3e772b79a9c51aa6016149c7a95fd5
Rao Dhanu <sumanraghu.rao@gmail.com>
2025-04-10 23:57:45 +0530

    Updated

--- frontend/src/brain/Brain.ts
+++ frontend/src/brain/Brain.ts
@@ -1,94 +1,17 @@
-import {
-  CheckHealthData,
-  DubaiQueryRequest,
-  GetGoogleMapsApiKeyData,
-  LocationQueryRequest,
-  ProcessDubaiQueryData,
-  ProcessDubaiQueryError,
-  QueryLocationData,
-  QueryLocationError,
-  StreamDubaiResponseData,
-  StreamDubaiResponseError,
-} from "./data-contracts";
-import { ContentType, HttpClient, RequestParams } from "./http-client";
+import { HandleHealthzData } from "./data-contracts";
+import { HttpClient, RequestParams } from "./http-client";
 
 export class Brain<SecurityDataType = unknown> extends HttpClient<SecurityDataType> {
   /**
-   * @description Check health of application. Returns 200 when OK, 500 when not.
+   * No description
    *
-   * @name check_health
-   * @summary Check Health
+   * @name handle_healthz
+   * @summary Handle Healthz
    * @request GET:/_healthz
    */
-  check_health = (params: RequestParams = {}) =>
-    this.request<CheckHealthData, any>({
+  handle_healthz = (params: RequestParams = {}) =>
+    this.request<HandleHealthzData, any>({
       path: `/_healthz`,
-      method: "GET",
-      ...params,
-    });
-
-  /**
-   * No description
-   *
-   * @tags dbtn/module:dubai_assistant
-   * @name process_dubai_query
-   * @summary Process Dubai Query
-   * @request POST:/routes/dubai-assistant/query
-   */
-  process_dubai_query = (data: DubaiQueryRequest, params: RequestParams = {}) =>
-    this.request<ProcessDubaiQueryData, ProcessDubaiQueryError>({
-      path: `/routes/dubai-assistant/query`,
-      method: "POST",
-      body: data,
-      type: ContentType.Json,
-      ...params,
-    });
-
-  /**
-   * @description Stream a response to a Dubai query for a more interactive experience
-   *
-   * @tags stream, dbtn/module:dubai_assistant
-   * @name stream_dubai_response
-   * @summary Stream Dubai Response
-   * @request POST:/routes/dubai-assistant/stream
-   */
-  stream_dubai_response = (data: DubaiQueryRequest, params: RequestParams = {}) =>
-    this.requestStream<StreamDubaiResponseData, StreamDubaiResponseError>({
-      path: `/routes/dubai-assistant/stream`,
-      method: "POST",
-      body: data,
-      type: ContentType.Json,
-      ...params,
-    });
-
-  /**
-   * No description
-   *
-   * @tags dbtn/module:dubai_locations
-   * @name query_location
-   * @summary Query Location
-   * @request POST:/routes/dubai-locations/query
-   */
-  query_location = (data: LocationQueryRequest, params: RequestParams = {}) =>
-    this.request<QueryLocationData, QueryLocationError>({
-      path: `/routes/dubai-locations/query`,
-      method: "POST",
-      body: data,
-      type: ContentType.Json,
-      ...params,
-    });
-
-  /**
-   * No description
-   *
-   * @tags dbtn/module:google_maps
-   * @name get_google_maps_api_key
-   * @summary Get Google Maps Api Key
-   * @request GET:/routes/api-key
-   */
-  get_google_maps_api_key = (params: RequestParams = {}) =>
-    this.request<GetGoogleMapsApiKeyData, any>({
-      path: `/routes/api-key`,
       method: "GET",
       ...params,
     });
--- frontend/src/brain/BrainRoute.ts
+++ frontend/src/brain/BrainRoute.ts
@@ -1,85 +1,17 @@
-import {
-  CheckHealthData,
-  DubaiQueryRequest,
-  GetGoogleMapsApiKeyData,
-  LocationQueryRequest,
-  ProcessDubaiQueryData,
-  QueryLocationData,
-  StreamDubaiResponseData,
-} from "./data-contracts";
+import { HandleHealthzData } from "./data-contracts";
 
 export namespace Brain {
   /**
-   * @description Check health of application. Returns 200 when OK, 500 when not.
-   * @name check_health
-   * @summary Check Health
+   * No description
+   * @name handle_healthz
+   * @summary Handle Healthz
    * @request GET:/_healthz
    */
-  export namespace check_health {
+  export namespace handle_healthz {
     export type RequestParams = {};
     export type RequestQuery = {};
     export type RequestBody = never;
     export type RequestHeaders = {};
-    export type ResponseBody = CheckHealthData;
-  }
-
-  /**
-   * No description
-   * @tags dbtn/module:dubai_assistant
-   * @name process_dubai_query
-   * @summary Process Dubai Query
-   * @request POST:/routes/dubai-assistant/query
-   */
-  export namespace process_dubai_query {
-    export type RequestParams = {};
-    export type RequestQuery = {};
-    export type RequestBody = DubaiQueryRequest;
-    export type RequestHeaders = {};
-    export type ResponseBody = ProcessDubaiQueryData;
-  }
-
-  /**
-   * @description Stream a response to a Dubai query for a more interactive experience
-   * @tags stream, dbtn/module:dubai_assistant
-   * @name stream_dubai_response
-   * @summary Stream Dubai Response
-   * @request POST:/routes/dubai-assistant/stream
-   */
-  export namespace stream_dubai_response {
-    export type RequestParams = {};
-    export type RequestQuery = {};
-    export type RequestBody = DubaiQueryRequest;
-    export type RequestHeaders = {};
-    export type ResponseBody = StreamDubaiResponseData;
-  }
-
-  /**
-   * No description
-   * @tags dbtn/module:dubai_locations
-   * @name query_location
-   * @summary Query Location
-   * @request POST:/routes/dubai-locations/query
-   */
-  export namespace query_location {
-    export type RequestParams = {};
-    export type RequestQuery = {};
-    export type RequestBody = LocationQueryRequest;
-    export type RequestHeaders = {};
-    export type ResponseBody = QueryLocationData;
-  }
-
-  /**
-   * No description
-   * @tags dbtn/module:google_maps
-   * @name get_google_maps_api_key
-   * @summary Get Google Maps Api Key
-   * @request GET:/routes/api-key
-   */
-  export namespace get_google_maps_api_key {
-    export type RequestParams = {};
-    export type RequestQuery = {};
-    export type RequestBody = never;
-    export type RequestHeaders = {};
-    export type ResponseBody = GetGoogleMapsApiKeyData;
+    export type ResponseBody = HandleHealthzData;
   }
 }
